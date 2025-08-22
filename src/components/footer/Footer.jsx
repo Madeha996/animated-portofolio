1971976838e4424c1e9aa76a1f4cb33a670457e7
madeha <madehayoussof96@gmail.com>
2025-08-22 15:48:59 +0300

    create leftSider footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Website, 22025, By Dev</p>
+    </footer>
+  );
+};
+
+export default Footer;
